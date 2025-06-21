1732be81ba5a6fbf6510c52a0185fcb246ba7a6a
vijay Thakur <practice66375@gmail.com>
2025-06-21 22:25:53 +0530

    sd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   import express from 'express';
   import cors from 'cors';
   import mongoose from 'mongoose';
-  import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
   import nodemailer from 'nodemailer';
   import twilio from 'twilio';
   import dotenv from 'dotenv';
